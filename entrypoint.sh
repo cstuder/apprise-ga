52ec43a5d1c620f78885f229e611fb5edf46ab8a
Christian Studer <christian.studer2@swisscom.com>
2020-02-26 16:54:42 +0100

    Entrypoint: Applies best practices by shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 set -eu
 
-# Apprise GitHub Action script
+### Apprise GitHub Action script
 #
-# @author Christian Studer <cstuder@existenz.ch>
+# Uses the environment variable `APPRISE_URL`.
+# 
+# Uses two CLI parameters `title` and `message`.
+#
+###
 
 # Save title and message to temporary files
 titlefile=$(mktemp)
 messagefile=$(mktemp)
 
-echo $1 > $titlefile
-echo $2 > $messagefile
+echo "$1" > "$titlefile"
+echo "$2" > "$messagefile"
 
 # Apply templating with data from the GitHub event
-title=$(j2 $titlefile $GITHUB_EVENT_PATH)
-message=$(j2 $messagefile $GITHUB_EVENT_PATH)
+title=$(j2 "$titlefile" "$GITHUB_EVENT_PATH")
+message=$(j2 "$messagefile" "$GITHUB_EVENT_PATH")
 
-# Uses the Apprise CLI tool
+# Uses the Apprise CLI tool to send the notifciation
 apprise -t "$title" -b "$message" "$APPRISE_URL"
